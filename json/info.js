e86c69fc5cf078fda968f0f1426bf7d9c0eff3a9
root <root@ip-172-31-25-166.ap-northeast-1.compute.internal>
2015-05-16 11:16:46 +0000

    外部JSON読み込み完了

--- json/info.js
+++ json/info.js
@@ -1,26 +1,16 @@
-$.ajax({
-    url: 'http://ec2-52-68-154-199.ap-northeast-1.compute.amazonaws.com/users.json',
-    type: 'GET',
-    dataType: "jsonp",
-    jsonp: 'callback',
-    timeout: 10000,
-    success: function(user){
-        $(user).each(function(){
-          $('<tr>'+
-            '<th>'+this.id+'</th>'+
-            '<td>'+this.name+'</td>'+
-            '<td>'+this.comment+'</td>'+
-            '</tr>').appendTo('table.tbl tbody');
-          })
-    },
-    error: function(user) {
-        //取得失敗時の処理
-      alert('error');
-    },
-    complete: function(user) {
-        //最後に実行する処理
-      alert('complete!');
-    }
+$(function(){
+
+	//HTMLを初期化
+	$("table.tbl tbody").html("");
+
+	//HTMLを生成
+	$.getJSON("http://ec2-52-68-154-199.ap-northeast-1.compute.amazonaws.com/users.json", function(data){
+		$(data).each(function(){
+			$('<tr>'+
+			  '<th>'+this.name+'</th>'+
+			  '<td class="label"><span class="' + this.star + '">' +
+			  this.comment + '</span></td>'+
+		'</tr>').appendTo('table.tbl tbody');
+		})
+	})
 });
-
-
